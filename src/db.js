2f8e8f3110f3e9daf2d69bf887538984a4db296f
Marcos Aurélio Oripka <aureliomarkos@yahoo.com>
2024-08-19 09:45:03 -0300

    alteração arquivo db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,30 @@
 require('dotenv').config({ path: '../src/mysqlConfig.env' });
 const mysql = require('mysql2');
 
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10, // Ajuste conforme necessário
+  queueLimit: 0,  
+  timezone: '+00:00'
+});
+
+connection.getConnection((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados: ', err);
+    return;
+  }
+  console.log('Conectado ao banco de dados MySQL');
+});
+
+module.exports = connection.promise();
+
+
+/*
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -19,28 +43,4 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
-
-
-/*
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10, // Ajuste conforme necessário
-  queueLimit: 0,  
-  timezone: '+00:00'
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados: ', err);
-    return;
-  }
-  console.log('Conectado ao banco de dados MySQL');
-});
-
-module.exports = connection.promise();
 */
