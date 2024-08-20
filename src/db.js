a8dbf3f4f027f7657b1a0b0fe4e9319b4f27200c
Marcos Aurélio Oripka <aureliomarkos@yahoo.com>
2024-08-20 15:59:04 -0300

    alteração arquivos server.js

--- src/db.js
+++ src/db.js
@@ -2,29 +2,6 @@
 require('dotenv').config({ path: '../src/mysqlConfig.env' });
 const mysql = require('mysql2');
 
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10, // Ajuste conforme necessário
-  queueLimit: 0,  
-  timezone: '+00:00'
-});
-
-connection.getConnection((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados: ', err);
-    return;
-  }
-  console.log('Conectado ao banco de dados MySQL');
-});
-
-module.exports = connection.promise();
-
-/*
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -43,4 +20,4 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
-*/
+
